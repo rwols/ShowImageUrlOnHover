99517c7829b809cea0c04a559e74f22af8f962ea
Raoul Wols <raoulwols@gmail.com>
2017-03-26 14:40:54 +0200

    Fixes for relative paths

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
+<!-- This is a plugin for Sublime Text 3. -->
+
 # ShowImageUrlOnHover
-Sublime Text 3 plugin -- Shows the image of the URL on mouse hover
+
+Shows the image of a URL on mouse hover.
+
+# Usage
+
+Hover your mouse over a URL, wether in source code or in plain text or in a
+comment. A popup will appear with the image displayed.
+
--- ShowImageUrlOnHover.py
+++ ShowImageUrlOnHover.py
@@ -24,7 +24,8 @@ class ShowImageUrlOnHover(sublime_plugin.ViewEventListener):
     def on_hover(self, point, hover_zone):
         if hover_zone != sublime.HOVER_TEXT:
             return
-        if self.view.match_selector(point, 'comment') or self.view.match_selector(point, 'text'):
+        if (self.view.match_selector(point, 'comment') or 
+            self.view.match_selector(point, 'text.plain')):
             self.url = self.extract_from_line_in_plaintext(point)
         else:
             self.url = self.extract_from_scope(point)
@@ -47,21 +48,45 @@ class ShowImageUrlOnHover(sublime_plugin.ViewEventListener):
         url = self.view.substr(region)
         return dequote(url)
 
+    def ensure_abs_path(self):
+        if not os.path.isabs(self.url):
+            # Turn it into an absolute path.
+            path = self.view.file_name()
+            if not path:
+                return
+            path = os.path.join(os.path.dirname(path), self.url)
+            self.url = os.path.abspath(path)
+
     def download_image(self):
         parsed_url = urllib.parse.urlparse(self.url)
         # For some reason, png files from the web are not displayed.
         # gifs also give problems.
-        if parsed_url.scheme == 'https' or self.ext == '.png' or self.ext == '.gif':
-            self.temppath = os.path.join(tempfile.mkdtemp(), os.path.basename(self.url))
-            try:
-                self.temppath, headers = urllib.request.urlretrieve(self.url, self.temppath)
-            except Exception as e:
-                print(str(e))
-                return
-            self.show_popup('file://' + self.temppath)
+        if (parsed_url.scheme == 'https' or 
+            self.ext == '.png' or 
+            self.ext == '.gif'):
+            if parsed_url.scheme:
+                tempdir = tempfile.mkdtemp()
+                basename = os.path.basename(self.url)
+                self.tmppath = os.path.join(tempdir, basename)
+                try:
+                    from urllib.request import urlretrieve
+                    self.tmppath, headers = urlretrieve(self.url, self.tmppath)
+                except Exception as e:
+                    print(str(e))
+                    return
+                self.show_popup('file://' + self.tmppath)
+            else:
+                self.ensure_abs_path()
+                self.tmppath = None
+                self.show_popup('file://' + self.url)
         elif parsed_url.scheme in ('http', 'file', 'res', 'data'):
-            self.temppath = None
+            self.tmppath = None
             self.show_popup(self.url)
+        elif not parsed_url.scheme:
+            # Assume it's a local file
+            self.ensure_abs_path()
+            self.tmppath = None
+            self.show_popup('file://' + self.url)
 
     def show_popup(self, src):
         content = '<html><body><img src="{}"></body></html>'.format(src)
@@ -79,16 +104,19 @@ class ShowImageUrlOnHover(sublime_plugin.ViewEventListener):
         pass
 
     def on_hide(self):
-        if not self.temppath:
+        if not self.tmppath:
             return
-        os.remove(self.temppath)
-        self.temppath = None
+        try:
+            os.remove(self.tmppath)
+        except FileNotFoundError as e:
+            print(str(e))
+        self.tmppath = None
 
 # These are test strings. Uncomment and hover over them to test them.
 # test_large_png = 'http://bellard.org/bpg/3.png'
 # test_large_jpg = 'https://upload.wikimedia.org/wikipedia/commons/8/8c/Chess_Large.JPG'
 # test_large_jpeg = 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Dds40-097_large.jpeg'
 # test_animated_gif = 'http://netdna.webdesignerdepot.com/uploads/2013/07/icons-animation.gif'
-
+# non_existing = 'asdf.jpg'
 
 
